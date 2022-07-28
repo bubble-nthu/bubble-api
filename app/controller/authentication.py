8b2c921f2f469c627adefc794aaf1582ce1f78be
Huang Chuan <river1440>
2022-07-28 23:50:36 +0800

    fix: remove token.py

--- app/controller/authentication.py
+++ app/controller/authentication.py
@@ -3,10 +3,11 @@ from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from ..models import User
 from . import api
 from .errors import error_response, forbidden , unauthorized
+from app import db
+from app.services.authenticate_user import AuthenticateUser
 
 basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth()
-
 
 @basic_auth.verify_password
 def verify_password(email, password):
@@ -18,6 +19,20 @@ def verify_password(email, password):
 def basic_auth_error(status):
     return error_response(status)
 
+@api.route('/auth/authenticate', methods=['POST'])
+@basic_auth.login_required
+def get_token():
+    # POST /api/v1/auth/authenticate
+    auth_user = AuthenticateUser.call(basic_auth.current_user())
+    response = {'data': auth_user}
+    return jsonify(response)
+
+@api.route('/auth/authenticate', methods=['DELETE'])
+@token_auth.login_required
+def revoke_token():
+    token_auth.current_user().revoke_token()
+    db.session.commit()
+    return '', 204
 
 @token_auth.verify_token
 def verify_token(token):
